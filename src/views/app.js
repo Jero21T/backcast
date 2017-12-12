1bb677ba2e0f5299566ef3dc089616b87d948a41
Hack Reactor Student <student@hackreactor.com>
2017-12-11 21:26:19 -0800

    working on videolist view

--- src/views/app.js
+++ src/views/app.js
@@ -3,15 +3,22 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.render();
   },
 
 
   render: function() {
+    this.videos = new Videos(window.exampleVideoData);
+    this.videoPlayerView = new VideoPlayerView();
+    this.videoListView = new VideoListView({collection: this.videos});
+    
     this.$el.html(this.template());
+    this.$el.append(this.videoPlayerView);
+    this.$el.append(this.videoListView);
     return this;
   },
 
   template: templateURL('src/templates/app.html')
 
 });
+
